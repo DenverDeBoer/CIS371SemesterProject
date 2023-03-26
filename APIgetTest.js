21d7befc0397bc72323ac5e1e412438086f4f146
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2023-03-26 13:57:57 -0400

    API testing

--- APIgetTest.js
+++ APIgetTest.js
@@ -1,17 +1,31 @@
-const test = require('./APITest.js')
-const options = {
-    host: 'api.edamam.com',
-    port: 443,
-    path: '/api/recipes/v2?type=public&q=chicken%20alfredo&app_id=93b6ea7f&app_key=d579a7606055c5329149fae116174ee2&ingr=5',
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}
+//const test = require('./APITest.js')
+//const options = {
+//    host: 'api.edamam.com',
+//    port: 443,
+//    path: '/api/recipes/v2?type=public&q=chicken%20alfredo&app_id=93b6ea7f&app_key=d579a7606055c5329149fae116174ee2&ingr=5',
+//    method: 'GET',
+//    headers: {
+//        'Content-Type': 'application/json'
+//    }
+//}
 
-test.getJSON(options, (statusCode, result) => {
-    console.log('onResult: (${statusCode})\n\n${JSON.stringify(result)}')
+//test.getJSON(options, (statusCode, result) => {
+//    console.log('onResult: (${statusCode})\n\n${JSON.stringify(result)}')
     
-    res.statusCode = statusCode
-    res.send(result)
+//    res.statusCode = statusCode
+//    res.send(result)
+//})
+
+const request = require('request')
+
+request("https://api.edamam.com/api/recipes/v2?type=public&q=chicken%20alfredo&app_id=93b6ea7f&app_key=d579a7606055c5329149fae116174ee2&ingr=5&field=ingredientLines",
+function(error, response, body) {
+    //console.log(body) prints raw json data
+    var replies = JSON.parse(body)
+    //replies.length is undefined hence for loop doesn't execute
+    console.log(replies.hits.length)
+    for(var i = 0; i < replies.length; i++) {
+        var r = replies.hits[i]
+        console.log(r)
+    }
 })
